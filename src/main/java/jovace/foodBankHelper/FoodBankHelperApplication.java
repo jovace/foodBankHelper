16084301ed56fa244a0b582612556de4d7a9f5a3
jovace <jorge.vazquez.acevedo@alumnos.upm.es>
2020-08-07 16:02:37 +0200

    Inicial

---
+++ src/main/java/jovace/foodBankHelper/FoodBankHelperApplication.java
@@ -0,0 +1,13 @@
+package jovace.foodBankHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBankHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBankHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jovace/foodBankHelper/FoodBankHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package jovace.foodBankHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBankHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
